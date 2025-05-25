67ec831418e92dfa3ec0ff419edf8623460d240b
Igbemo <aymayorr@gmail.com>
2025-05-25 21:46:02 +0100

    Modified with values

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,10 +1,10 @@
 #This programme will perform simple arithmethic
 
 #Values assigned to variables
-number1 = 20
-number2 = 40
+number1 = 10
+number2 = 5
 
 #The print function should give the output
 print("Addition of ",number2, "and ", number1, "is" , number2 + number1)
-print("Subraction of ",number2, "and ", number1, "is" , number2 - number1)
+print("Subraction of ",number2, "and ", number1, "is" , number1 - number2)
 print("Multiplication of ",number2, "and ", number1, "is" , number2 * number1)
