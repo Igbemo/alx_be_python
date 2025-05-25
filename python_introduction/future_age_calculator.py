b615c32e78b96d6f35dbfde8bf9810066b5867fb
Igbemo <aymayorr@gmail.com>
2025-05-25 22:59:19 +0100

    mathematics tells us your age in 2025 future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,6 +1,6 @@
 #Purpose @2023: it calculates your age in 2025
 
-user_age_in_2023 = int(input("How old are you ? "))
+user_age_in_2023 = int(input("How old are you? "))
 age = user_age_in_2023 + 27
 
 #your age will be printed on the screen screen
