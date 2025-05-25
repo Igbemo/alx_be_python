7e5023ce84e68cb5ebc0e42a738969f63ddff6ee
Igbemo <aymayorr@gmail.com>
2025-05-25 21:38:56 +0100

    Performing simple arithmetic with python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 20
+number2 = 40
+print("Addition of ",number2, "and ", number1, "is" , number2 + number1)
+print("Subraction of ",number2, "and ", number1, "is" , number2 - number1)
+print("Multiplication of ",number2, "and ", number1, "is" , number2 * number1)
