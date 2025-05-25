7cd13a660c7e549ab2c74bde0d106b8e525f3645
Igbemo <aymayorr@gmail.com>
2025-05-25 22:02:40 +0100

    Corrections

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 #This programme will calculate simple interest (I = P * R * T)
 
-principal = P = 1000
-rate = R = 0.05
-time = T = 3
-I = P * R * T
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
 
 #This function should give the simple interest
-print("The simple interest is :", I)
+print("The simple interest is :", interest)
  
