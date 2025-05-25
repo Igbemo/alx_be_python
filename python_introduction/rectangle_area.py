eed006a381c9aedb9ddb3293ffc43e49419392a5
Igbemo <aymayorr@gmail.com>
2025-05-25 22:21:12 +0100

    The mathematics of shapes

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,8 @@
+#This programme will calculate the area of a rectangle
+
+length = 10
+width = 5
+Area = length * width
+
+#This function prints the result on the screen
+print("The area of a rectangle is: ",Area)
