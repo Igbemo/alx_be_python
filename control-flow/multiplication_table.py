6dcdf5dd16bba334f00070ba38fc541da7ce8fac
Igbemo <aymayorr@gmail.com>
2025-07-08 15:26:54 +0100

    my multiplication tables

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+#this programme prints a multiplication table of any number you entered
+
+number = int(input("Enter a number to see its multiplication table: ")) #Enter a number to see its table
+X = number
+for Y in range(1,13):
+    Z = Y * X
+    print(f"{X} x {Y} = {Z}" )
