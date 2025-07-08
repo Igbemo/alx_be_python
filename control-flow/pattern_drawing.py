920fcb862a72d9436495c6428bb97e409bcac75c
Igbemo <aymayorr@gmail.com>
2025-07-08 07:18:50 +0100

    My updated size of pattern

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,11 +1,10 @@
-i=0
 # This programme draws a simple text-based pattern.
-j = int(input("Enter the size of the pattern: "))
-while i < 4:
+i=0
+x = int(input("Enter the size of the pattern: ")) #This value creates the number of rows amd for the outer loop
+while i < x:
     j = 0
     while j < 4:
-        print ( "*" ,end = "")
+        print ( "*" ,end = "") #this prints tbe value of j in four columns, horizontal *
         j = j +1
     print()
     i = i+1
-    
