154387fe0c423075f73148d699cde86b6b128055
Igbemo <aymayorr@gmail.com>
2025-07-08 07:29:38 +0100

    My daily reminder app

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,9 +1,9 @@
 #This programme helps prioritise task amd a daily reminder using match case
 task = input("Enter your task: ")                            #Take the task as a string value
 priority = input("Priority (high/medium/low): ")    #priotise th pe task
-time = input("Is it time-bound? (yes/no): ")            #holds the time as a boolean value
+time_bound = input("Is it time-bound? (yes/no): ")            #holds the time as a boolean value
 
-if time =="yes":
+if time_bound=="yes":
     match priority:
         case "high":
             print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
